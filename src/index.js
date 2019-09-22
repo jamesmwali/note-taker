439e5e6d5d0820f72287e668bd0cfbc01931504f
James Mwali <james@blacklight.co.za>
2019-09-22 23:40:07 +0200

    created redux store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './redux/reducers';
+import {createStore, applyMiddleware} from 'redux'; // ? Create a redux store;
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// * Redux store imports
+
+// ? Create a redux store
+// ! Store requires reducers -> actions
+// * applyMiddleware requires thunk
+
+// ! Step 1 - Create store variable
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+// ! Step 2 - provide the store reducers to react
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App/>
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
