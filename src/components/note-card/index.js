8912e39f22a7e47ca0d391b421a3bb8b341a04da
James Mwali <james@blacklight.co.za>
2019-09-22 23:38:00 +0200

    added notes childrens wrapper

---
+++ src/components/note-card/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NoteCard = ({children}) => {
+
+  return (
+      <div className="jumbotron">
+        <div>
+          {children}
+        </div>
+      </div>
+  );
+};
+
+export default NoteCard;
